7a3d199825577cebbd009581d737c45ffe553eb0
dotnet noobie <dotnetnoobie@gmail.com>
2020-07-22 12:43:06 +1000

    Add project files.

---
+++ BlazorMvc/BlazorMvc.Server/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMvc.Server.Controllers
+{
+    [Route("[controller]")]
+    public class DemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlazorMvc/BlazorMvc.Server/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMvc.Server.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
